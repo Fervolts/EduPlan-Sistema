912ec3f71663db689f2fa043337965c0f84775c4
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2024-07-21 01:40:58 -0400

    Agregando modelos de tablas
    Se implementaron los modelos de las tablas con la que estaremos trabajando

---
+++ backend/models/administrador.js
@@ -0,0 +1,28 @@
+const  DataTypes  = require('sequelize');
+const sequelize = require('../config/database');
+
+const Administrador = sequelize.define('Administrador', {
+  id_administrador: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  usuario: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  contrasena: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'administradores',
+  timestamps: false,
+});
+
+module.exports = Administrador;
