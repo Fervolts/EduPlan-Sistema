ea45b610f7745c6925b2021204a8fb7c6e3885d1
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2024-07-21 03:52:59 -0400

    Update Comprar.jsx

--- frontend/EDUPLAN/src/components/Comprar.jsx
+++ frontend/EDUPLAN/src/components/Comprar.jsx
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './styles/Comprar.css';
+import Video1 from '../assets/Video.mp4';
+
 const Comprar = () => {
-  const [modelos, setModelos] = useState([]);
   const [formulario, setFormulario] = useState({
-    nombre: localStorage.getItem('username') || '',
-    apellido: '',
-    correo: '',
-    modelo: '',
+    nombres: localStorage.getItem('username') || '',
+    apellidos: '',
+    correo_electronico: '',
+    documento_identidad: '',
+    numero_telefono: '',
+    usuario: '',
+    contrasena: '',
     estado: 'En espera',
     enviado: false, 
   });
   const [errores, setErrores] = useState({});
-
-  useEffect(() => {
-    fetch('http://localhost:3000/modelos')
-      .then(response => response.json())
-      .then(data => setModelos(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
 
   const handleChange = (event) => {
     setFormulario({
@@ -31,74 +28,85 @@ const Comprar = () => {
 
     let errores = {};
 
-    // Validar los campos aquí
-    if (!formulario.nombre.trim()) {
-      errores.nombre = "El campo nombre es obligatorio";
+    if (!formulario.nombres.trim()) {
+      errores.nombres = "El campo nombres es obligatorio";
     }
-    if (!formulario.apellido.trim()) {
-      errores.apellido = "El campo apellido es obligatorio";
+    if (!formulario.apellidos.trim()) {
+      errores.apellidos = "El campo apellidos es obligatorio";
     }
-    if (!formulario.correo.trim()) {
-      errores.correo = "El campo correo es obligatorio";
+    if (!formulario.correo_electronico.trim()) {
+      errores.correo_electronico = "El campo correo electronico es obligatorio";
     }
 
     setErrores(errores);
 
-    if (Object.keys(errores).length === 0) {
-      fetch('http://localhost:3000/compra', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formulario),
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        setFormulario({ ...formulario, enviado: true }); 
-        setTimeout(() => window.location.reload(), 2000); // Agregado
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    }
+    fetch('http://localhost:3000/api/register/estudiante', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formulario),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+      setFormulario({ ...formulario, enviado: true });
+      setTimeout(() => window.location.reload(), 2000);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
 
   return (
-    <div className="tarjeta">
-
-            <video autoPlay loop muted className='video1'>
-                <source src={Video1} type="video/mp4" />
-            </video>
-      <form className="formulario" onSubmit={handleSubmit}>
-        <label>
-          Nombre:
-          <input type="text" name="nombre" value={formulario.nombre} readOnly />
-          {errores.nombre && <p className="error">{errores.nombre}</p>}
+    <div className="card">
+      <video autoPlay loop muted className="video">
+        <source src={Video1} type="video/mp4" />
+      </video>
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="label">
+          Nombres:
+          <input type="text" name="nombres" value={formulario.nombres} onChange={handleChange} className="input" />
+          {errores.nombres && <p className="error">{errores.nombres}</p>}
         </label>
-        <label>
+        <label className="label">
           Apellido:
-          <input type="text" name="apellido" onChange={handleChange} />
-          {errores.apellido && <p className="error">{errores.apellido}</p>}
+          <input type="text" name="apellidos" value={formulario.apellidos} onChange={handleChange} className="input" />
+          {errores.apellidos && <p className="error">{errores.apellidos}</p>}
         </label>
-        <label>
-          Correo:
-          <input type="email" name="correo" onChange={handleChange} />
-          {errores.correo && <p className="error">{errores.correo}</p>}
+        <label className="label">
+          Correo electrónico:
+          <input type="email" name="correo_electronico" value={formulario.correo_electronico} onChange={handleChange} className="input" />
+          {errores.correo_electronico && <p className="error">{errores.correo_electronico}</p>}
         </label>
-        <label>
-          Modelo a comprar:
-          <select name="modelo" onChange={handleChange}>
-            {modelos.map((modelo, index) => (
-              <option key={index} value={modelo.id}>
-                {modelo.Modelo}
-              </option>
-            ))}
-          </select>
+        <label className="label">
+          Documento Identidad:
+          <input type="text" name="documento_identidad" value={formulario.documento_identidad} onChange={handleChange} className="input" />
+          {errores.documento_identidad && <p className="error">{errores.documento_identidad}</p>}
         </label>
-        <button type="submit">Comprar</button>
+        <label className="label">
+          Número teléfono:
+          <input type="text" name="numero_telefono" value={formulario.numero_telefono} onChange={handleChange} className="input" />
+          {errores.numero_telefono && <p className="error">{errores.numero_telefono}</p>}
+        </label>
+        <label className="label">
+          Usuario:
+          <input type="text" name="usuario" value={formulario.usuario} onChange={handleChange} className="input" />
+          {errores.usuario && <p className="error">{errores.usuario}</p>}
+        </label>
+        <label className="label">
+          Contraseña:
+          <input type="password" name="contrasena" value={formulario.contrasena} onChange={handleChange} className="input" />
+          {errores.contrasena && <p className="error">{errores.contrasena}</p>}
+        </label>
+        <button type="submit" className="button">Comprar</button>
       </form>
-      {formulario.enviado && <p className="mensaje-exito">Formulario enviado correctamente!</p>} {/* Agregado */}
+      {formulario.enviado && <p className="success-message">Formulario enviado correctamente!</p>}
     </div>
   );
 };
