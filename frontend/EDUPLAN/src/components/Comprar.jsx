e8442aeae0ac0bd086b1e295a44369094f8fd8b6
Esteban Gomez <egomez@opensur.com>
2024-07-21 01:18:17 -0400

    Ajustes a lo subido por Fernando. Form Estudiantes

--- frontend/EDUPLAN/src/components/Comprar.jsx
+++ frontend/EDUPLAN/src/components/Comprar.jsx
@@ -4,10 +4,13 @@ import Video1 from '../assets/CArrito.mp4';
 const Comprar = () => {
   const [modelos, setModelos] = useState([]);
   const [formulario, setFormulario] = useState({
-    nombre: localStorage.getItem('username') || '',
-    apellido: '',
-    correo: '',
-    modelo: '',
+    nombres: localStorage.getItem('username') || '',
+    apellidos: '',
+    correo_electronico: '',
+    documento_identidad: '',
+    numero_telefono: '',
+    usuario: '',
+    contrasena: '',
     estado: 'En espera',
     enviado: false, 
   });
@@ -33,14 +36,14 @@ const Comprar = () => {
     let errores = {};
 
     // Validar los campos aquí
-    if (!formulario.nombre.trim()) {
-      errores.nombre = "El campo nombre es obligatorio";
+    if (!formulario.nombres.trim()) {
+      errores.nombres = "El campo nombres es obligatorio";
     }
-    if (!formulario.apellido.trim()) {
-      errores.apellido = "El campo apellido es obligatorio";
+    if (!formulario.apellidos.trim()) {
+      errores.apellidos = "El campo apellidos es obligatorio";
     }
-    if (!formulario.correo.trim()) {
-      errores.correo = "El campo correo es obligatorio";
+    if (!formulario.correo_electronico.trim()) {
+      errores.correo_electronico = "El campo correo electronico es obligatorio";
     }
 
     setErrores(errores);
@@ -66,41 +69,51 @@ const Comprar = () => {
   };
 
   return (
-    <div className="tarjeta">
+<div className="card">
+    <video autoPlay loop muted className="video">
+        <source src={Video1} type="video/mp4" />
+    </video>
+    <form className="form" onSubmit={handleSubmit}>
+        <label className="label">
+            Nombres:
+            <input type="text" name="nombres" value={formulario.nombres} readOnly className="input" />
+            {errores.nombres && <p className="error">{errores.nombres}</p>}
+        </label>
+        <label className="label">
+            Apellido:
+            <input type="text" name="apellidos" onChange={handleChange} className="input" />
+            {errores.apellidos && <p className="error">{errores.apellidos}</p>}
+        </label>
+        <label className="label">
+            Correo electrónico:
+            <input type="email" name="correo_electronico" onChange={handleChange} className="input" />
+            {errores.correo_electronico && <p className="error">{errores.correo_electronico}</p>}
+        </label>
+        <label className="label">
+            Documento Identidad:
+            <input type="text" name="documento_identidad" onChange={handleChange} className="input" />
+            {errores.documento_identidad && <p className="error">{errores.documento_identidad}</p>}
+        </label>
+        <label className="label">
+            Número teléfono:
+            <input type="text" name="numero_telefono" onChange={handleChange} className="input" />
+            {errores.numero_telefono && <p className="error">{errores.numero_telefono}</p>}
+        </label>
+        <label className="label">
+            Usuario:
+            <input type="text" name="usuario" onChange={handleChange} className="input" />
+            {errores.usuario && <p className="error">{errores.usuario}</p>}
+        </label>
+        <label className="label">
+            Contraseña:
+            <input type="password" name="contrasena" onChange={handleChange} className="input" />
+            {errores.contrasena && <p className="error">{errores.contrasena}</p>}
+        </label>
+        <button type="submit" className="button">Comprar</button>
+    </form>
+    {formulario.enviado && <p className="success-message">Formulario enviado correctamente!</p>}
+</div>
 
-            <video autoPlay loop muted className='video1'>
-                <source src={Video1} type="video/mp4" />
-            </video>
-      <form className="formulario" onSubmit={handleSubmit}>
-        <label>
-          Nombre:
-          <input type="text" name="nombre" value={formulario.nombre} readOnly />
-          {errores.nombre && <p className="error">{errores.nombre}</p>}
-        </label>
-        <label>
-          Apellido:
-          <input type="text" name="apellido" onChange={handleChange} />
-          {errores.apellido && <p className="error">{errores.apellido}</p>}
-        </label>
-        <label>
-          Correo:
-          <input type="email" name="correo" onChange={handleChange} />
-          {errores.correo && <p className="error">{errores.correo}</p>}
-        </label>
-        <label>
-          Modelo a comprar:
-          <select name="modelo" onChange={handleChange}>
-            {modelos.map((modelo, index) => (
-              <option key={index} value={modelo.id}>
-                {modelo.Modelo}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button type="submit">Comprar</button>
-      </form>
-      {formulario.enviado && <p className="mensaje-exito">Formulario enviado correctamente!</p>} {/* Agregado */}
-    </div>
   );
 };
 
