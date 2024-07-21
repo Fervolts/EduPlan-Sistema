692652089ee2856a412f4fe1d02db35377e2fd1c
Esteban Gomez <egomez@opensur.com>
2024-07-21 04:46:50 -0400

    Ajustes de estilos

--- frontend/EDUPLAN/src/components/Inicio.jsx
+++ frontend/EDUPLAN/src/components/Inicio.jsx
@@ -9,17 +9,18 @@ function Inicio() {
     <div className = 'Caja'>
       <img src={Logo} className="ImageFondo" />
       
-       <div className='section'>
-      <InstagramEmbed url="https://www.instagram.com/p/C9pwEevxKo3/" width={350} />
+      <div className='section'>
+        <InstagramEmbed url="https://www.instagram.com/p/C9pwEevxKo3/" width={350} />
       </div> 
+
       <div className='section2'>
-      <div className= 'inside'>
-      <h1 className='titulito'>Bienvenidos</h1>
-      <p className='parrafo'>
-        Los autos deportivos son un estilo de vida que se basa en el uso de
-        tecnología para mejorar la velocidad, la eficiencia y la seguridad.
-      </p>
-      </div>
+        <div className= 'inside'>
+          <h1 className='titulito'>Bienvenidos</h1>
+          <p className='parrafo'>
+            Los autos deportivos son un estilo de vida que se basa en el uso de
+            tecnología para mejorar la velocidad, la eficiencia y la seguridad.
+          </p>
+        </div>
       </div>
 
       <div className='fotico2'>
